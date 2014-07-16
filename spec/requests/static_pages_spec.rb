851c0e972c505af32109df81b97f5b7b1835db4d
huayan <huayan.gao@dragonicity.com>
2014-07-16 13:37:19 +0800

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,15 +2,17 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
+  let (:base_title) {"Ruby on Rails Tutorial Sample App"}
+
   describe "Home page" do
- 		it "should have the content 'Sample App'" do
+ 		it "should have the content 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+      expect(page).to have_content('Home')
     end
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title(" #{base_title} | Home")
     end
   end
 
@@ -23,7 +25,7 @@ describe "Static Pages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title(" #{base_title} | Help")
     end
   end
 
@@ -36,8 +38,22 @@ describe "Static Pages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title(" #{base_title} | About Us")
   	end
 	end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+
+    it "should have the title 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title(" #{base_title} | Contact Us")
+  	end
+	end
+
 
 end
